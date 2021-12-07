fd15be61b0e7c729b8b304712a563b2a53452da8
hampusnasstrom <hampus.nasstrom@gmail.com>
2021-12-07 12:49:45 +0100

    Added documentation for detectors.py router

--- src/fastmda/routers/detectors.py
+++ src/fastmda/routers/detectors.py
@@ -12,30 +12,23 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.DetectorInfo])
+@router.get("/", response_model=List[schemas.DetectorInfo], summary="Get all added detector instances")
 async def get_all_detectors(device_id: int = Path(..., description="The ID of the device.")):
+    """
+    Get a list of information for all the added detector instances for the specified device.
+    """
     try:
         return [det.info for _, det in device_dict[device_id].detectors.items()]
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No device with ID {device_id}.")
 
 
-@router.get("/{detector_id}", response_model=schemas.DetectorInfo)
+@router.get("/{detector_id}", response_model=schemas.DetectorInfo, summary="Get detector information")
 async def get_detector(device_id: int = Path(..., description="The ID of the device."),
                        detector_id: int = Path(..., description="The ID of the detector.")):
-    try:
-        device = device_dict[device_id]
-    except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No device with ID {device_id}.")
-    try:
-        return device.detectors[detector_id].info
-    except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No detector with ID {detector_id}.")
-
-
-@router.get("/{detector_id}", response_model=schemas.DetectorInfo)
-async def get_detector(device_id: int = Path(..., description="The ID of the device."),
-                       detector_id: int = Path(..., description="The ID of the detector.")):
+    """
+    Get the information for the specified detector of the the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -47,9 +40,13 @@ async def get_detector(device_id: int = Path(..., description="The ID of the dev
 
 
 @router.get("/{detector_id}/settings",
-            response_model=List[Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo]])
+            response_model=List[Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo]],
+            summary="Get all detector settings")
 async def get_all_detector_settings(device_id: int = Path(..., description="The ID of the device."),
                                     detector_id: int = Path(..., description="The ID of the detector.")):
+    """
+    Get a list of information on all the settings for the specified detector of the the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -61,10 +58,14 @@ async def get_all_detector_settings(device_id: int = Path(..., description="The 
 
 
 @router.get("/{detector_id}/setting/{setting_id}",
-            response_model=Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo])
+            response_model=Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo],
+            summary="Get detector setting information")
 async def get_detector_setting(device_id: int = Path(..., description="The ID of the device."),
                                detector_id: int = Path(..., description="The ID of the detector."),
                                setting_id: int = Path(..., description="The ID of the setting.")):
+    """
+    Get the setting information for the specified setting for the specified detector of the the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -79,10 +80,14 @@ async def get_detector_setting(device_id: int = Path(..., description="The ID of
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No detector setting with ID {setting_id}.")
 
 
-@router.get("/{detector_id}/setting/{setting_id}/value", response_model=Union[str, float])
+@router.get("/{detector_id}/setting/{setting_id}/value", response_model=Union[str, float],
+            summary="Get detector setting value")
 async def get_detector_setting_value(device_id: int = Path(..., description="The ID of the device."),
                                      detector_id: int = Path(..., description="The ID of the detector."),
                                      setting_id: int = Path(..., description="The ID of the setting.")):
+    """
+    Get the setting value for the specified setting for the specified detector of the the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -97,13 +102,17 @@ async def get_detector_setting_value(device_id: int = Path(..., description="The
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No detector setting with ID {setting_id}.")
 
 
-@router.put("/{detector_id}/setting/{setting_id}/value", response_model=Union[str, float])
+@router.put("/{detector_id}/setting/{setting_id}/value", response_model=Union[str, float],
+            summary="Set detector setting value")
 async def set_detector_setting_value(
         value: Union[int, float] = Query(..., description="The value to set the setting to."),
         device_id: int = Path(..., description="The ID of the device."),
         detector_id: int = Path(..., description="The ID of the detector."),
         setting_id: int = Path(..., description="The ID of the setting.")
 ):
+    """
+    Set the setting value for the specified setting for the specified detector of the the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
