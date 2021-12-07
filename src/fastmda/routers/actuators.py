10e468675175259b6fa4c2985e950ffb6ab47729
hampusnasstrom <hampus.nasstrom@gmail.com>
2021-12-07 15:00:44 +0100

    Added documentation to the actuators.py router

--- src/fastmda/routers/actuators.py
+++ src/fastmda/routers/actuators.py
@@ -1,12 +1,11 @@
-from typing import Dict, List, Union
+from typing import List, Union
 
-from fastapi import APIRouter, Path, HTTPException, status, Depends, Query
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Path, HTTPException, status, Query
 
-from fastmda import crud, schemas
+from fastmda import schemas
 from fastmda.database import SessionLocal
 from fastmda.exceptions import *
-from fastmda.globals import device_dict, device_types_info, device_types_modules
+from fastmda.globals import device_dict
 from fastmda.objects import DiscreteActuator, ContinuousActuator
 
 router = APIRouter(
@@ -24,25 +23,35 @@ def get_db():
         db.close()
 
 
-@router.get("/discrete", response_model=List[schemas.DiscreteActuatorInfo])
+@router.get("/discrete", response_model=List[schemas.DiscreteActuatorInfo], summary="Get all discrete actuators")
 async def get_all_discrete_actuators(device_id: int = Path(..., description="The ID of the device.")):
+    """
+    Get a list of information for all discrete actuators of the specified device.
+    """
     try:
         return [act.info for _, act in device_dict[device_id].actuators.items() if isinstance(act, DiscreteActuator)]
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No device with ID {device_id}.")
 
 
-@router.get("/continuous", response_model=List[schemas.ContinuousActuatorInfo])
+@router.get("/continuous", response_model=List[schemas.ContinuousActuatorInfo], summary="Get all continuous actuators")
 async def get_all_continuous_actuators(device_id: int = Path(..., description="The ID of the device.")):
+    """
+    Get a list of information for all continuous actuators of the specified device.
+    """
     try:
         return [act.info for _, act in device_dict[device_id].actuators.items() if isinstance(act, ContinuousActuator)]
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No device with ID {device_id}.")
 
 
-@router.get("/{actuator_id}", response_model=Union[schemas.DiscreteActuatorInfo, schemas.ContinuousActuatorInfo])
+@router.get("/{actuator_id}", response_model=Union[schemas.DiscreteActuatorInfo, schemas.ContinuousActuatorInfo],
+            summary="Get actuator information")
 async def get_actuator(device_id: int = Path(..., description="The ID of the device."),
                        actuator_id: int = Path(..., description="The ID of the actuator.")):
+    """
+    Get the information for the specified actuator of the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -53,9 +62,12 @@ async def get_actuator(device_id: int = Path(..., description="The ID of the dev
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No actuator with ID {actuator_id}.")
 
 
-@router.get("/{actuator_id}/value", response_model=Union[str, float])
+@router.get("/{actuator_id}/value", response_model=Union[str, float], summary="Get actuator value")
 async def get_actuator_value(device_id: int = Path(..., description="The ID of the device."),
                              actuator_id: int = Path(..., description="The ID of the actuator.")):
+    """
+    Get the value for the specified actuator of the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -66,10 +78,13 @@ async def get_actuator_value(device_id: int = Path(..., description="The ID of t
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No actuator with ID {actuator_id}.")
 
 
-@router.put("/{actuator_id}/value", response_model=Union[str, float])
+@router.put("/{actuator_id}/value", response_model=Union[str, float], summary="Set actuator value")
 async def set_actuator_value(value: Union[int, float] = Query(..., description="The value to set the actuator to."),
                              device_id: int = Path(..., description="The ID of the device."),
                              actuator_id: int = Path(..., description="The ID of the actuator.")):
+    """
+    Set the value for the specified actuator of the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -92,9 +107,13 @@ async def set_actuator_value(value: Union[int, float] = Query(..., description="
 
 
 @router.get("/{actuator_id}/settings",
-            response_model=List[Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo]])
+            response_model=List[Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo]],
+            summary="Get all actuator settings")
 async def get_all_actuator_settings(device_id: int = Path(..., description="The ID of the device."),
                                     actuator_id: int = Path(..., description="The ID of the actuator.")):
+    """
+    Get a list of information on all the settings for the specified detector of the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -106,10 +125,14 @@ async def get_all_actuator_settings(device_id: int = Path(..., description="The 
 
 
 @router.get("/{actuator_id}/setting/{setting_id}",
-            response_model=Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo])
+            response_model=Union[schemas.DiscreteSettingInfo, schemas.ContinuousSettingInfo],
+            summary="Get actuator setting information")
 async def get_actuator_setting(device_id: int = Path(..., description="The ID of the device."),
                                actuator_id: int = Path(..., description="The ID of the actuator."),
                                setting_id: int = Path(..., description="The ID of the setting.")):
+    """
+    Get the setting information for the specified setting for the specified actuator of the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -124,10 +147,14 @@ async def get_actuator_setting(device_id: int = Path(..., description="The ID of
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No actuator setting with ID {setting_id}.")
 
 
-@router.get("/{actuator_id}/setting/{setting_id}/value", response_model=Union[str, float])
+@router.get("/{actuator_id}/setting/{setting_id}/value", response_model=Union[str, float],
+            summary="Get actuator setting value")
 async def get_actuator_setting_value(device_id: int = Path(..., description="The ID of the device."),
                                      actuator_id: int = Path(..., description="The ID of the actuator."),
                                      setting_id: int = Path(..., description="The ID of the setting.")):
+    """
+    Get the setting value for the specified setting for the specified actuator of the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
@@ -142,13 +169,17 @@ async def get_actuator_setting_value(device_id: int = Path(..., description="The
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No actuator setting with ID {setting_id}.")
 
 
-@router.put("/{actuator_id}/setting/{setting_id}/value", response_model=Union[str, float])
+@router.put("/{actuator_id}/setting/{setting_id}/value", response_model=Union[str, float],
+            summary="Set actuator setting value")
 async def set_actuator_setting_value(
         value: Union[int, float] = Query(..., description="The value to set the setting to."),
         device_id: int = Path(..., description="The ID of the device."),
         actuator_id: int = Path(..., description="The ID of the actuator."),
         setting_id: int = Path(..., description="The ID of the setting.")
 ):
+    """
+    Set the setting value for the specified setting for the specified actuator of the specified device.
+    """
     try:
         device = device_dict[device_id]
     except KeyError:
